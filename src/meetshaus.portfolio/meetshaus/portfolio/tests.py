2753c94b61e387bc1b18fafe05ee2ee7aeafe262
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-02-26 16:58:23 +0100

    Include new package for use cases and example works.

---
+++ src/meetshaus.portfolio/meetshaus/portfolio/tests.py
@@ -0,0 +1,62 @@
+import doctest
+import unittest
+
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase.layer import onsetup
+
+import meetshaus.portfolio
+
+OPTION_FLAGS = doctest.NORMALIZE_WHITESPACE | \
+               doctest.ELLIPSIS
+
+ptc.setupPloneSite(products=['meetshaus.portfolio'])
+
+
+class TestCase(ptc.PloneTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            zcml.load_config('configure.zcml',
+              meetshaus.portfolio)
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='meetshaus.portfolio',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='meetshaus.portfolio.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        # Integration tests that use PloneTestCase
+        ztc.ZopeDocFileSuite(
+            'INTEGRATION.txt', 
+            package='meetshaus.portfolio',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+        #ztc.FunctionalDocFileSuite(
+        #    'BROWSER.TXT',
+        #    package='meetshaus.portfolio',
+        #    optionflags = OPTION_FLAGS,
+        #    test_class=TestCase),
+
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
