cd96da5cb471ba4c3e7256258c057731dc731ebf
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-03-11 16:08:20 +0100

    Pep8 cleanup for blog folder dexterity file

--- src/meetshaus.blog/meetshaus/blog/blog.py
+++ src/meetshaus.blog/meetshaus/blog/blog.py
@@ -2,9 +2,8 @@ from five import grok
 from Acquisition import aq_inner
 import calendar
 from DateTime import DateTime
-from plone.directives import dexterity, form
+from plone.directives import form
 from zope import schema
-from z3c.form import group, field
 from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.PloneBatch import Batch
@@ -24,7 +23,6 @@ class IBlog(form.Schema):
     """
     Blog Content folder.
     """
-    
     form.widget(bio=WysiwygFieldWidget)
     bio = schema.Text(
         title=_(u'About the Author'),
@@ -32,30 +30,31 @@ class IBlog(form.Schema):
         required=False,
     )
 
+
 class BlogView(grok.View):
     grok.context(IBlog)
     grok.require('zope2.View')
     grok.name('blog-view')
-    
+
     def blogitems(self):
         """List all blog items as brains"""
-        year = int(self.request.form.get('year',0))
-        month = int(self.request.form.get('month',0))
+        year = int(self.request.form.get('year', 0))
+        month = int(self.request.form.get('month', 0))
         subject = self.request.form.get('category', None)
         return self.get_entries(year=year, month=month, subject=subject)
-    
+
     def batch(self):
         b_size = 5
         b_start = self.request.form.get('b_start', 0)
         return Batch(self.blogitems(), b_size, b_start, orphan=1)
-    
+
     def commentsEnabled(self, ob):
         if USE_PAD:
             conversation = IConversation(ob)
             return conversation.enabled()
         else:
             return self.portal_discussion.isDiscussionAllowedFor(ob)
-    
+
     def commentCount(self, ob):
         if USE_PAD:
             conversation = IConversation(ob)
@@ -63,16 +62,15 @@ class BlogView(grok.View):
         else:
             discussion = self.portal_discussion.getDiscussionFor(ob)
             return discussion.replyCount(ob)
-    
-    
+
     def _base_query(self):
         context = aq_inner(self.context)
         obj_provides = IBlogEntry.__identifier__
         path = '/'.join(context.getPhysicalPath())
-        return dict(path={'query': path, 'depth':1},
+        return dict(path={'query': path, 'depth': 1},
                     object_provides=obj_provides,
                     sort_on='effective', sort_order='reverse')
-    
+
     def get_entries(self, year=None, month=None, subject=None):
         context = aq_inner(self.context)
         catalog = getToolByName(context, 'portal_catalog')
