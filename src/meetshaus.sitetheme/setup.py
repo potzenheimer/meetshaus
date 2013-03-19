6aea79d6b201d466d8767ac3730f835b96cdfa75
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-03-19 01:35:27 +0100

    Work in progress new theme UI

--- src/meetshaus.sitetheme/setup.py
+++ src/meetshaus.sitetheme/setup.py
@@ -27,6 +27,7 @@ setup(name='meetshaus.sitetheme',
           'setuptools',
           'plone.app.theming',
           'plone.app.themingplugins',
+          'plone.app.toolbar',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
