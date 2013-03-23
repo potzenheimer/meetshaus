bb9e8144d0c0b8ce34d468eac579fff1c0b8d63f
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-03-23 15:28:58 +0100

    Include recent blog entries in the landing page view

--- src/meetshaus.landingpage/meetshaus/landingpage/browser/landingpage.py
+++ src/meetshaus.landingpage/meetshaus/landingpage/browser/landingpage.py
@@ -1,10 +1,26 @@
-
 from five import grok
-from Acquisition import aq_inner
-from Products.CMFCore.utils import getToolByName
+from plone import api
+
+from plone.app.contentlisting.interfaces import IContentListing
+
+from meetshaus.blog.blogentry import IBlogEntry
 from meetshaus.landingpage.content.landingpage import ILandingPage
+
 
 class View(grok.View):
     grok.context(ILandingPage)
     grok.require('zope2.View')
     grok.name('view')
+
+    def update(self):
+        self.has_blogentries = len(self.blogentries()) > 0
+
+    def blogentries(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        items = catalog(object_provides=IBlogEntry.__identifier__,
+                        review_state='published',
+                        sort_on='effective',
+                        sort_order='reverse',
+                        limit=3)[:3]
+        results = IContentListing(items)
+        return results
