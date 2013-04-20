cfd7e0884f97e705ad420bd55945d2c3f15b4435
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-04-20 17:17:38 +0200

    IImageScale use to cure rfc822 type error

--- src/meetshaus.blog/meetshaus/blog/blogentry.py
+++ src/meetshaus.blog/meetshaus/blog/blogentry.py
@@ -5,12 +5,14 @@ from plone.directives import form
 
 from plone.app.textfield import RichText
 
+from plone.namedfile.interfaces import IImageScaleTraversable
+
 from meetshaus.blog import MessageFactory as _
 
 
 # Interface class; used to define content-type schema.
 
-class IBlogEntry(form.Schema):
+class IBlogEntry(form.Schema, IImageScaleTraversable):
     """
     A single blog entry for the rgd blog
     """
@@ -30,8 +32,8 @@ class BlogEntryView(grok.View):
         context = aq_inner(self.context)
         parent = aq_parent(context)
         try:
-            if getattr(parent, 'getId', None) is None or \
-                   parent.getId() == 'talkback':
+            if (getattr(parent, 'getId', None) is None or
+                    parent.getId() == 'talkback'):
                 # Skip any Z3 views that may be in the acq tree;
                 # Skip past the talkback container if that's where we are
                 parent = aq_parent(aq_inner(parent))
