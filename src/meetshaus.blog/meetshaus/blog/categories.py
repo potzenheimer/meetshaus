28b6e711da7470aadce9a1362c5a60227fc7268a
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-12-04 10:55:55 +0100

    Add category datamanager setup

--- src/meetshaus.blog/meetshaus/blog/categories.py
+++ src/meetshaus.blog/meetshaus/blog/categories.py
@@ -1,9 +1,15 @@
 # -*- coding: UTF-8 -*-
 """ Module listing available and asigned blog categories """
+import json
+import time
 import urllib2
-from plone import api
-from five import grok
+
 from Products.CMFCore.interfaces import IContentish
+from five import grok
+from plone import api
+from plone.app.layout.navigation.interfaces import INavigationRoot
+from zope.component import getUtility
+from plone.i18n.normalizer.interfaces import IIDNormalizer
 
 from meetshaus.blog.blogentry import IBlogEntry
 
@@ -33,3 +39,62 @@ class BlogCategories(grok.View):
         sub = urllib2.quote(subject.encode('utf-8'))
         url = '{0}/blog?category={1}'.format(portal_url, sub)
         return url
+
+
+class SetupBlogCategoryStorage(grok.View):
+    grok.context(INavigationRoot)
+    grok.require('cmf.ManagePortal')
+    grok.name('setup-blog-category-storage')
+
+    def keywords(self):
+        catalog = self.catalog()
+        keywords = catalog.uniqueValuesFor('Subject')
+        keywords = [unicode(k, 'utf-8') for k in keywords]
+        return keywords
+
+    def _normalize_keyword(self, keyword):
+        normalizer = getUtility(IIDNormalizer)
+        return normalizer.normalize(keyword)
+
+    def _count_entries(self, keyword):
+        catalog = self.catalog()
+        brains = catalog(object_provides=IBlogEntry.__identifier__,
+                         Subject=keyword.encode('utf-8'))
+        return len(brains)
+
+    def _build_archive_url(self, keyword):
+        portal_url = api.portal.get().absolute_url()
+        sub = urllib2.quote(keyword.encode('utf-8'))
+        url = '{0}/blog?category={1}'.format(portal_url, sub)
+        return url
+
+    def _process_request(self):
+        api_url = self.request.get('ACTUAL_URL')
+        data = {
+            'url': api_url,
+            'timestamp': str(int(time.time())),
+        }
+        items = list()
+        for kw in self.keywords():
+            info = {
+                'id': self._normalize_keyword(kw),
+                'url': self._build_archive_url(kw),
+                'count': self._count_entries(kw),
+                'title': kw,
+                'description': ''
+            }
+            items.append(info)
+        data['items'] = items
+        return data
+
+    def render(self):
+        start = time.time()
+        data = self._process_request()
+        end = time.time()
+        data.update(dict(_runtime=end-start))
+        json_data = json.dumps(data)
+        api.portal.set_registry_record(
+            'meetshaus.blog.interfaces.IBlogToolSettings.blog_categories',
+            json_data)
+        next_url = api.portal.get().absolute_url()
+        return self.request.response.redirect(next_url)
