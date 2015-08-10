f0f52a0a918dd0de0a0571d7397425ecd44539ed
Christoph Boehner <cb@vorwaerts-werbung.de>
2015-08-10 22:54:25 +0200

    Start folderish blogentry migration (wip)

--- src/meetshaus.blog/meetshaus/blog/helper.py
+++ src/meetshaus.blog/meetshaus/blog/helper.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """Module providing migration to folderish posts"""
 from Acquisition import aq_inner
+from Products.CMFCore.interfaces import IContentish
 from five import grok
 from plone import api
 from zope.lifecycleevent import modified
-from Products.CMFCore.interfaces import IContentish
 
 from meetshaus.blog.blogentry import IBlogEntry
+from meetshaus.blog import MessageFactory as _
 
 
 class FolderishPostMigration(grok.View):
@@ -15,7 +16,12 @@ class FolderishPostMigration(grok.View):
     grok.name('migrate-to-folderish')
 
     def render(self):
-        pass
+        context = aq_inner(self.context)
+        next_url = context.absolute_url()
+        created_idx = self._create_posts()
+        msg = _(u"{0} new postings created".format(created_idx))
+        api.portal.show_message(message=msg, request=self.request)
+        return self.request.response.redirect(next_url)
 
     def _create_posts(self):
         context = aq_inner(self.context)
