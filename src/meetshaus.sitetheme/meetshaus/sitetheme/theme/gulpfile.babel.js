0ad3c1abcc5f38cbcf6d073dea32a17b3a97d7fc
Christoph Böhner-Figas <boehner@team23.de>
2018-03-27 23:42:51 +0200

    Update theme build tasks
    - define browser sync base tasks
    - serve and watch development tasks
    - organize build tasks

--- src/meetshaus.sitetheme/meetshaus/sitetheme/theme/gulpfile.babel.js
+++ src/meetshaus.sitetheme/meetshaus/sitetheme/theme/gulpfile.babel.js
@@ -2,9 +2,6 @@
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import {create as bsCreate} from 'browser-sync';
-import del from 'del';
-import revDel from 'rev-del';
-import args from 'yargs';
 import hub from 'gulp-hub';
 
 hub([
@@ -21,73 +18,83 @@ hub([
 
 const $ = gulpLoadPlugins();
 const browserSync = bsCreate();
-
 var runSequence = require('run-sequence');
 
-var cp = require('child_process');
-var es = require('event-stream');
-var pkg = require('./package.json');
+// Load configuration
 var cfg = require('./config.json');
-var fs = require('fs');
 
 
-gulp.task('dev:browser-sync', function () {
+
+/*
+ * TASKS
+ *
+ */
+
+// Browser sync
+gulp.task('bs:reload', function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('bs:reload:styles', ['styles:dist'], function() {
+    return global.browserSync.reload('*.css');
+});
+
+
+// Development tasks
+gulp.task('dev:serve', ['build:dist:base'], () => {
     browserSync.init({
         notify: false,
         port: 9499,
         server: {
-            baseDir: ['.tmp', cfg.paths.dist],
-            routes: {
-                '/scripts': cfg.paths.dist + 'scripts',
-                '/styles': cfg.paths.dist + 'styles',
-                '/assets': cfg.paths.dist + 'assets',
-            }
+            baseDir: [cfg.paths.dist]
         }
     });
+
+    gulp.watch([
+        cfg.paths.dist + '/*.html',
+        cfg.paths.dist + '/scripts/*.js',
+        cfg.paths.dist + '/styles/*.css',
+    ]).on('change', browserSync.reload);
+
+    gulp.watch(cfg.paths.app + "sass/**/*.scss", ['styles:dist']);
+    gulp.watch(cfg.paths.app + "scripts/**/*.js", ['collect:scripts:app']);
+    gulp.watch(cfg.paths.app + "**/*.html", ['jekyll:build', 'collect:html', 'bs:reload']);
 });
 
-gulp.task('dev:bs-reload', function () {
-    browserSync.reload();
-});
-
-
-gulp.task('dev:serve', ['build:dist:base'], () => {
+gulp.task('dev:serve:background', function () {
     browserSync.init({
-    notify: false,
-    port: 9499,
-    server: {
-        baseDir: [cfg.paths.base + cfg.paths.dist]
-    }
-});
+        notify: false,
+        port: 9499,
+        open: false,
+        server: {
+            baseDir: [cfg.paths.dist]
+        }
+    });
+    gulp.watch([
+        cfg.paths.dist + '/*.html',
+        cfg.paths.dist + '/scripts/*.js',
+        cfg.paths.dist + '/styles/*.css',
+    ]).on('change', browserSync.reload);
 
-gulp.watch([
-    cfg.paths.app + '/*.html',
-    cfg.paths.app + '/scripts/*.js',
-    cfg.paths.app + '/styles/*.css',
-]).on('change', browserSync.reload);
-
-gulp.watch(cfg.paths.app + "sass/**/*.scss", ['styles']);
-gulp.watch(cfg.paths.app + "scripts/**/*.js", ['scripts']);
-gulp.watch(cfg.paths.app + "{,*/}*.html", ['jekyll:build', 'html']);
-});
-
-gulp.task('default', ['dev:browser-sync'], function () {
-    gulp.watch(cfg.paths.app + "sass/**/*.scss", ['styles']);
-    gulp.watch(cfg.paths.app + "scripts/**/*.js", ['scripts']);
-    gulp.watch(cfg.paths.app + "*.html", ['bs-reload']);
+    gulp.watch(cfg.paths.app + "sass/**/*.scss", ['styles:dist']);
+    gulp.watch(cfg.paths.app + "scripts/**/*.js", ['collect:scripts:app']);
+    gulp.watch(cfg.paths.app + "**/*.html", ['jekyll:build', 'collect:html', 'bs:reload']);
 });
 
 
-gulp.task('dev:watch:styles', ['styles'], function() {
-  return global.browserSync.reload('*.css');
+gulp.task('dev:watch', ['dev:browser-sync'], function () {
+    gulp.watch(cfg.paths.app + "sass/**/*.scss", ['styles:dist']);
+    gulp.watch(cfg.paths.app + "scripts/**/*.js", ['collect:scripts:app']);
+    gulp.watch(cfg.paths.app + "**/*.html", ['jekyll:build', 'collect:html', 'bs:reload']);
+});
+
+gulp.task('dev:watch:styles', function () {
+    gulp.watch(cfg.paths.app + "sass/**/*.scss", ['bs:reload:styles']);
 });
 
 
-gulp.task('dev:watch', function () {
-    gulp.watch(cfg.paths.app + "sass/**/*.scss", ['dev:watch:styles']);
-});
-
-
+// Build tasks
 gulp.task('build:init', function(done) {
     runSequence(
         'clean:dist',
@@ -135,6 +142,16 @@ gulp.task('build:pat', function(done) {
         done);
 });
 
+gulp.task('build:dist:base', function(done) {
+    runSequence(
+        'build:base:dist',
+        'replace:base',
+        'revision:styles',
+        'replace:revision:styles',
+        'collect:html',
+        done);
+});
+
 gulp.task('build:dist:full', function(done) {
     runSequence(
         'build:init',
@@ -146,25 +163,14 @@ gulp.task('build:dist:full', function(done) {
         done);
 });
 
-gulp.task('build:dist:base', function(done) {
-    runSequence(
-        'build:base:dist',
-        'replace:base',
-        'revision:styles',
-        'replace:revision:styles',
-        'collect:html',
-        done);
-});
 
-// Top level tasks
+// Base tasks
+
 // Clean distribution directory and start over
 gulp.task('init', ['build:init']);
 
 // Run development build
 gulp.task('develop', ['build:dev']);
-
-// Development build usable with standalone Plone backend
-gulp.task('pat', ['build:pat']);
 
 // Build distribution versions of styles and scripts
 gulp.task('dist', ['build:dist:base']);
@@ -172,6 +178,14 @@ gulp.task('dist', ['build:dist:base']);
 // Rebuild whole theme for distribution
 gulp.task('build', ['build:dist:full']);
 
+// Development build usable with standalone Plone backend
+gulp.task('pat', ['build:pat']);
+
+// Development Server
+gulp.task('serve', ['dev:serve']);
+
+gulp.task('watch', ['dev:watch'])
+
 // Start working with the styles
-gulp.task('default', ['watch']);
+gulp.task('default', ['dev:watch:styles']);
 
