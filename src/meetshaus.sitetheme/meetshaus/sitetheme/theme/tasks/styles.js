57f5a9372e0eaf6ec9f145ce71ee5d02e9c49385
Christoph Böhner-Figas <boehner@team23.de>
2019-01-02 22:27:38 +0100

    Update theme
    Massive changes to the theme infrastructure including
    
    - rfs support
    - default layout fixes

--- src/meetshaus.sitetheme/meetshaus/sitetheme/theme/tasks/styles.js
+++ src/meetshaus.sitetheme/meetshaus/sitetheme/theme/tasks/styles.js
@@ -59,7 +59,34 @@ export function stylesDev() {
 
 stylesDev.description = 'Compile stylesheet from scss partials';
 
+// Styles build task
+export function stylesEditor(cb) {
+    pump([
+        gulp.src(cfg.paths.app + 'scss/wysiwyg.scss'),
+        $.plumber(),
+        $.sourcemaps.init(),
+        $.sass.sync({
+            outputStyle: 'expanded',
+            precision: 10,
+            includePaths: [cfg.paths.src]
+        }).on('error', $.sass.logError),
+        $.autoprefixer({browsers: ['last 4 version']}),
+        gulp.dest(cfg.paths.dist + 'styles/'),
+        $.cssnano(),
+        $.rename({
+            basename: 'wysiwyg',
+            suffix: '.min'
+        }),
+        $.sourcemaps.write(),
+        gulp.dest(cfg.paths.dist + 'styles/'),
+        browserSync.reload({stream: true})
+    ], cb);
+};
+
+stylesEditor.description = 'Compile stylesheet for wysiwyg editor';
+
 
 // Stylesheet builds
 gulp.task('styles:dev', stylesDev);
 gulp.task('styles:dist', styles);
+gulp.task('styles:editor', stylesEditor);
