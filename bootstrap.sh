f13afbd89bf2ccf019aae7750fbd051dd19f6797
Christoph Böhner-Figas <boehner@team23.de>
2020-08-19 16:36:43 +0200

    [UPGRADE] Move to Python5.2/Python2
    Use intermediate step prior to actually performing the python
    version upgrade

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-virtualenv -p python2.7 --clear .
+virtualenv -p `which python2.7` .
 env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" ./bin/pip install cryptography
 ./bin/pip install -r requirements.txt
 ./bin/buildout $*
