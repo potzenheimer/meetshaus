3caa508a8bcb4751c1612f37366abd3b0b332e76
Kreativkombinat GbR <info@kreativkombinat.de>
2020-10-16 22:26:00 +0200

    Deploying '9bbdfc19f43e9d7b37f84bd9cbabf2521cf9de5b' from branch 'production' into 'release/production'. Date: 2020-10-16T22:26:00.130574

--- src/meetshaus.blog/meetshaus/blog/blog.py
+++ src/meetshaus.blog/meetshaus/blog/blog.py
@@ -1,29 +1,18 @@
 # -*- coding: UTF-8 -*-
 """ Module providing folderish blog content type """
-import calendar
-from Acquisition import aq_inner
-from DateTime import DateTime
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.PloneBatch import Batch
-from five import grok
-from plone import api
-from plone.app.contentlisting.interfaces import IContentListing
-from plone.app.discussion.interfaces import IConversation
-from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
-from plone.directives import form
-from plone.event.utils import pydt
+from plone.dexterity.content import Container
+from plone.supermodel import model
 from zope import schema
-
-from meetshaus.blog.blogentry import IBlogEntry
+from zope.interface import implementer
 
 from meetshaus.blog import MessageFactory as _
 
 
-class IBlog(form.Schema):
+class IBlog(model.Schema):
     """
     Blog Content folder.
     """
-    form.widget(bio=WysiwygFieldWidget)
+    # form.widget(bio=WysiwygFieldWidget)
     bio = schema.Text(
         title=_(u'About the Author'),
         description=_(u'A short bio about the author of the blog.'),
@@ -31,85 +20,6 @@ class IBlog(form.Schema):
     )
 
 
-class BlogView(grok.View):
-    grok.context(IBlog)
-    grok.require('zope2.View')
-    grok.name('blog-view')
-
-    def blogitems(self):
-        """List all blog items as brains"""
-        year = int(self.request.form.get('year', 0))
-        month = int(self.request.form.get('month', 0))
-        subject = self.request.form.get('category', None)
-        return self.get_entries(year=year, month=month, subject=subject)
-
-    def batch(self):
-        b_size = 10
-        b_start = self.request.form.get('b_start', 0)
-        items = self.blogitems()[1:]
-        return Batch(items, b_size, b_start, orphan=1)
-
-    def commentsEnabled(self, ob):
-        conversation = IConversation(ob)
-        return conversation.enabled()
-
-    def commentCount(self, ob):
-        conversation = IConversation(ob)
-        return len(conversation)
-
-    def _base_query(self):
-        context = aq_inner(self.context)
-        obj_provides = IBlogEntry.__identifier__
-        path = '/'.join(context.getPhysicalPath())
-        return dict(path={'query': path, 'depth': 1},
-                    object_provides=obj_provides,
-                    sort_on='effective', sort_order='reverse')
-
-    def get_entries(self, year=None, month=None, subject=None):
-        context = aq_inner(self.context)
-        catalog = getToolByName(context, 'portal_catalog')
-        query = self._base_query()
-        if subject:
-            query['Subject'] = subject
-        if year:
-            if month:
-                lastday = calendar.monthrange(year, month)[1]
-                startdate = DateTime(year, month, 1, 0, 0)
-                enddate = DateTime(year, month, lastday, 23, 59, 59)
-            else:
-                startdate = DateTime(year, 1, 1, 0, 0)
-                enddate = DateTime(year, 12, 31, 0, 0)
-            query['effective'] = dict(query=(startdate, enddate),
-                                      range='minmax')
-        results = catalog.searchResults(**query)
-        return IContentListing(results)
-
-    def timestamp(self, uid):
-        item = api.content.get(UID=uid)
-        date = item.effective()
-        date = pydt(date)
-        timestamp = {}
-        timestamp['day'] = date.strftime("%d")
-        timestamp['month'] = date.strftime("%B")
-        timestamp['year'] = date.strftime("%Y")
-        timestamp['date'] = date
-        return timestamp
-
-    def _readable_text(self, uid):
-        context = api.content.get(UID=uid)
-        meta = context.title + ' ' + context.description
-        if context.text:
-            html = context.text.raw
-            transforms = api.portal.get_tool(name='portal_transforms')
-            stream = transforms.convertTo('text/plain',
-                                          html,
-                                          mimetype='text/html')
-            text = stream.getData().strip()
-            body = meta + ' ' + text
-        return body
-
-    def reading_time(self, uid):
-        text = self._readable_text(uid)
-        text_count = len(text.split(' '))
-        rt = text_count / 200
-        return rt
+@implementer(IBlog)
+class BlogPost(Container):
+    pass
---
+++ src/meetshaus.blog/meetshaus/blog/upgrades.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+# Module providing version specific upgrade steps
+import logging
+
+from zope.globalrequest import getRequest
+from plone import api
+from plone.app.upgrade.utils import cleanUpSkinsTool
+
+default_profile = 'profile-meetshaus.blog:default'
+log = logging.getLogger(__name__)
+
+
+def remove_ploneformgen(context=None, check_linkintegrity=True):
+    portal = api.portal.get()
+    portal_types = api.portal.get_tool('portal_types')
+    portal_catalog = api.portal.get_tool('portal_catalog')
+    qi = api.portal.get_tool('portal_quickinstaller')
+
+    log.info('removing PloneFormGen')
+    old_types = [
+        'FormFolder',
+    ]
+    old_types = [i for i in old_types if i in portal_types]
+    for old_type in old_types:
+        for brain in portal_catalog(portal_type=old_type):
+            log.info(u'Removing {}!'.format(brain.getPath()))
+            api.content.delete(brain.getObject(), check_linkintegrity=check_linkintegrity)  # noqa: E501
+    try:
+        portal.manage_delObjects(['formgen_tool'])
+    except AttributeError:
+        pass
+    try:
+        portal.portal_properties.manage_delObjects(['ploneformgen_properties'])
+    except BadRequest:
+        pass
+
+    if qi.isProductInstalled('PloneFormGen'):
+        qi.uninstallProducts(['PloneFormGen'])
+
+    if qi.isProductInstalled('Products.PloneFormGen'):
+        qi.uninstallProducts(['Products.PloneFormGen'])
+
+
+def migrate_ATBTreeFolder(context=None):
+    """Replace very old containers for news, events and Members
+    """
+    from plone.portlets.interfaces import ILocalPortletAssignmentManager
+    from plone.portlets.interfaces import IPortletManager
+    from zope.component import getMultiAdapter
+    from zope.component import queryUtility
+
+    portal = api.portal.get()
+    # create new containers:
+    if not portal['Members'].__class__.__name__ == 'ATBTreeFolder':
+        log.info('Migrating ATBTreeFolder not needed')
+        return
+    log.info('Migrating ATBTreeFolders')
+    members_new = api.content.create(
+        container=portal,
+        type='Folder',
+        id='members_new',
+        title=u'Benutzer',
+    )
+    members_new.setOrdering('unordered')
+    members_new.setLayout('@@member-search')
+    # Block all right column portlets by default
+    manager = queryUtility(IPortletManager, name='plone.rightcolumn')
+    if manager is not None:
+        assignable = getMultiAdapter(
+            (members_new, manager),
+            ILocalPortletAssignmentManager
+        )
+        assignable.setBlacklistStatus('context', True)
+        assignable.setBlacklistStatus('group', True)
+        assignable.setBlacklistStatus('content_type', True)
+
+
+    for item in portal.Members.contentValues():
+        api.content.move(
+            source=item,
+            target=members_new,
+        )
+    api.content.delete(obj=portal['Members'], check_linkintegrity=False)
+    api.content.rename(obj=portal['members_new'], new_id='Members')
+
+
+def uninstall_archetypes(context=None):
+    portal = api.portal.get()
+    request = getRequest()
+    installer = api.content.get_view('installer', portal, request)
+    addons = [
+        'Archtypes',
+        'ATContentTypes',
+        'plone.app.referenceablebehavior',
+        'plone.app.blob',
+        'plone.app.imaging',
+    ]
+    for addon in addons:
+        if installer.is_product_installed(addon):
+            installer.uninstall_product(addon)
+
+
+def remove_archetypes_traces(context=None):
+    portal = api.portal.get()
+
+    # remove obsolete AT tools
+    tools = [
+        'portal_languages',
+        'portal_tinymce',
+        'kupu_library_tool',
+        'portal_factory',
+        'portal_atct',
+        'uid_catalog',
+        'archetype_tool',
+        'reference_catalog',
+        'portal_metadata',
+    ]
+    for tool in tools:
+        if tool not in portal.keys():
+            log.info('Tool {} not found'.format(tool))
+            continue
+        try:
+            portal.manage_delObjects([tool])
+            log.info('Deleted {}'.format(tool))
+        except Exception as e:
+            log.info(u'Problem removing {}: {}'.format(tool, e))
+            try:
+                log.info(u'Fallback to remove without permission_checks')
+                portal._delObject(tool)
+                log.info('Deleted {}'.format(tool))
+            except Exception as e:
+                log.info(u'Another problem removing {}: {}'.format(tool, e))
+
+
+def pack_database(context=None):
+    """Pack the database"""
+    portal = api.portal.get()
+    app = portal.__parent__
+    db = app._p_jar.db()
+    db.pack(days=0)
+
+
+def cleanup_in_plone52(context=None):
+    migrate_ATBTreeFolder()
+    uninstall_archetypes()
+    remove_archetypes_traces()
+    portal = api.portal.get()
+    cleanUpSkinsTool(portal)
+    qi = api.portal.get_tool('portal_quickinstaller')
+    # Fix diazo theme
+    qi.reinstallProducts(['plone.app.theming'])
+    # Fix issue where we cannot pack the DB after it was migrated to Python 3
+    qi.reinstallProducts(['plone.app.relationfield'])
+    pack_database()
+
+
+def upgrade_1001(setup):
+    # Update registry settings
+    setup.runImportStepFromProfile(default_profile, 'plone.app.registry')
+
+
+def upgrade_1002(setup):
+    # Cleanup installation
+    cleanup_in_plone52()
+
+
+def upgrade_1003(setup):
+    # Cleanup installation
+    remove_ploneformgen()
